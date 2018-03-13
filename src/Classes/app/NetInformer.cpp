547170052fe7aeaf5d37736e5d1745b570788b75
guliping <1058719078@qq.com>
2018-03-13 13:09:35 +0800

    请求新连接的时候，需要把之前的请求消息清空

--- src/Classes/app/NetInformer.cpp
+++ src/Classes/app/NetInformer.cpp
@@ -4,6 +4,7 @@
 void NetInformer::dealCustomMsg(Wrap::MSGINFO* msg){
 	if (msg){
 		if (msg->cmd == MSG_CONNECT_LOBBY){
+			clearCurWaitMsg();//清空当前的等待消息，理论上来说，我们上层应用在调用connect之前，最好先调用disconnect
 			DataConnect* p = (DataConnect*)msg->v;
 			NetApp::GetInstance()->getLobbyTunnel()->connectTo(p->ip, p->port, p->timeout);
 		}
