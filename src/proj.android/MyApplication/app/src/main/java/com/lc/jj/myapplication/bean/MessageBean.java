3136011c9e91d885bf59a32c90ce77656eb49887
guliping <1058719078@qq.com>
2018-06-06 13:46:43 +0800

    增加房间进出消息的定义

---
+++ src/proj.android/MyApplication/app/src/main/java/com/lc/jj/myapplication/bean/MessageBean.java
@@ -0,0 +1,7 @@
+package com.lc.jj.myapplication.bean;
+
+public class MessageBean {
+    public int type;//0 talkBean,1 RoomMsgBean
+    public TalkBean tb;
+    public RoomMsgBean rmb;
+}
---
+++ src/proj.android/MyApplication/app/src/main/java/com/lc/jj/myapplication/bean/RoomMsgBean.java
@@ -0,0 +1,7 @@
+package com.lc.jj.myapplication.bean;
+
+public class RoomMsgBean {
+    public int enter;
+    public String uid;
+    public int type = -1;
+}
